515a1acbc0099adba16635491038e00708157418
dolev146 <dolev146@gmail.com>
2022-05-24 09:41:33 +0300

    added main

--- sources/OrgChart.cpp
+++ sources/OrgChart.cpp
@@ -306,7 +306,7 @@ OrgChart &OrgChart::operator=(OrgChart other)
      * @brief move assignment operator
      * @param other
      */
-    
+
     swap(root_tree, other.root_tree);
     return *this;
 }
@@ -334,13 +334,133 @@ OrgChart::Node *OrgChart::find_n(const string &find, OrgChart::Node *node)
     return nullptr;
 }
 
-
-
 OrgChart::Node::Node(const string &value)
 {
 
     this->value = string(value);
 }
 
+// clang++-9 -std=c++2a -Wall -Wextra -Werror -g -o main OrgChart.cpp
 
-// clang++-9 -std=c++2a -Wall -Wextra -Werror -g -o OrgChart OrgChart.cpp
+int main()
+{
+
+    OrgChart chart2;
+    chart2.add_root("Owner");
+    chart2.add_root("New_Owner");
+    // CHECK_THROWS(chart2.add_sub("Owner", "General Manager"));        // Owner was replaced
+    // CHECK_THROWS(chart2.add_sub("Lebron James", "General_Manager")); // no such value
+    chart2.add_sub("New_Owner", "General_Manager");
+    chart2.add_sub("New_Owner", "Share_Holders");
+    chart2.add_sub("New_Owner", "Head_of_operations");
+    chart2.add_sub("General_Manager", "Head_Coach");
+    chart2.add_sub("General_Manager", "Assistant_Coach");
+    chart2.add_sub("Head_of_operations", "Team_Doctor");
+    chart2.add_sub("Head_of_operations", "Head_Scout");
+    chart2.add_sub("Share_Holders", "Sub_Share_Holder1");
+    chart2.add_sub("Share_Holders", "Sub_Share_Holder2");
+    // CHECK_THROWS(chart2.add_sub("head_of_operations", "Cheerleader")); // Head not head
+    chart2.add_sub("Head_Coach", "Captain");
+    chart2.add_sub("Team_Doctor", "Team_Physiotherapist");
+
+    // test level order
+    vector<string> lvl_order;
+    for (auto it = chart2.begin_level_order(); it != chart2.end_level_order(); ++it)
+    {
+        lvl_order.push_back(*it);
+        cout << *it << " ";
+    }
+    cout << endl;
+
+    if (lvl_order.at(0).compare("New_Owner"))
+    {
+        cout << "level order test failed 11111 new Owner" << endl;
+    }
+    if (lvl_order.at(1) != "General_Manager")
+    {
+        cout << "level order test failed General_Manager" << endl;
+    };
+    if (lvl_order.at(2) != "Share_Holders")
+    {
+        cout << "level order test failed Share_Holders" << endl;
+    };
+    if (lvl_order.at(3) != "Head_of_operations")
+    {
+        cout << "level order test failed Head_of_operations" << endl;
+    };
+    if (lvl_order.at(4) != "Head_Coach")
+    {
+        cout << "level order test failed Head_Coach" << endl;
+    };
+    if (lvl_order.at(5) != "Assistant_Coach")
+    {
+        cout << "level order test failed Assistant_Coach" << endl;
+    };
+    if (lvl_order.at(6) != "Sub_Share_Holder1")
+    {
+        cout << "level order test failed Sub_Share_Holder1 " << endl;
+    };
+    if (lvl_order.at(7) != "Sub_Share_Holder2")
+    {
+        cout << "level order test failed Sub_Share_Holder2" << endl;
+    };
+    if (lvl_order.at(8) != "Team_Doctor")
+    {
+        cout << "level order test failed Team_Doctor" << endl;
+    };
+    if (lvl_order.at(9) != "Head_Scout")
+    {
+        cout << "level order test failed Head_Scout" << endl;
+    };
+    if (lvl_order.at(10) != "Captain")
+    {
+        cout << "level order test failed Captain" << endl;
+    };
+
+    // lvl_order.clear();
+
+    // // add another field and check that it works
+    // CHECK_NOTHROW(chart2.add_sub("Assistant_Coach", "Water_Boy"));
+    // cout << chart2 << endl;
+    // // check default iterator
+    // for (auto name : chart2)
+    // {
+    //     lvl_order.push_back(name);
+    // }
+    // CHECK_EQ(lvl_order.at(0), "New_Owner");
+    // CHECK_EQ(lvl_order.at(1), "General_Manager");
+    // CHECK_EQ(lvl_order.at(2), "Share_Holders");
+    // CHECK_EQ(lvl_order.at(3), "Head_of_operations");
+    // CHECK_EQ(lvl_order.at(4), "Head_Coach");
+    // CHECK_EQ(lvl_order.at(5), "Assistant_Coach");
+    // CHECK_EQ(lvl_order.at(6), "Sub_Share_Holder1");
+    // CHECK_EQ(lvl_order.at(7), "Sub_Share_Holder2");
+    // CHECK_EQ(lvl_order.at(8), "Team_Doctor");
+    // CHECK_EQ(lvl_order.at(9), "Head_Scout");
+    // CHECK_EQ(lvl_order.at(10), "Captain");
+    // CHECK_EQ(lvl_order.at(11), "Water_Boy");
+    // CHECK_EQ(lvl_order.at(12), "Team_Physiotherapist");
+
+    // // test reverse level order
+    // vector<string> rv_lvl_order;
+    // for (auto it = chart2.begin_reverse_order(); it != chart2.reverse_order(); ++it)
+    // {
+    //     rv_lvl_order.push_back(*it);
+    // }
+    // CHECK_EQ(rv_lvl_order.at(0), "Captain");
+    // CHECK_EQ(rv_lvl_order.at(1), "Water_Boy");
+    // CHECK_EQ(rv_lvl_order.at(2), "Team_Physiotherapist");
+    // CHECK_EQ(rv_lvl_order.at(3), "Head_Coach");
+    // CHECK_EQ(rv_lvl_order.at(4), "Assistant_Coach");
+    // CHECK_EQ(rv_lvl_order.at(5), "Sub_Share_Holder1");
+    // CHECK_EQ(rv_lvl_order.at(6), "Sub_Share_Holder2");
+    // CHECK_EQ(rv_lvl_order.at(7), "Team_Doctor");
+    // CHECK_EQ(rv_lvl_order.at(8), "Head_Scout");
+    // CHECK_EQ(rv_lvl_order.at(9), "General_Manager");
+    // CHECK_EQ(rv_lvl_order.at(10), "Share_Holders");
+    // CHECK_EQ(rv_lvl_order.at(11), "Head_of_operations");
+    // CHECK_EQ(rv_lvl_order.at(12), "New_Owner");
+
+    // // Change root check that still works
+    // CHECK_NOTHROW(chart2.add_root("Stan_Kronke"));
+}
